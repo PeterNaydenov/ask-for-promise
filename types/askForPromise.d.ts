bf03f62b9e573367ac987883f8d79e46f16e13b1
Peter Naydenov <peter.naydenov@gmail.com>
2024-12-17 22:32:56 +0200

    Fix in d.ts file

--- types/askForPromise.d.ts
+++ types/askForPromise.d.ts
@@ -1,38 +1,34 @@
+/**
+ * Object with promise and related helper functions
+ */
+interface AskObject {
+    promise?: Promise<any>;
+    promises?: AskObject[] | null;
+    done: (response?: any) => void;
+    cancel: (response?: any) => void;
+    each: (cbFn: Function, ...args: any[]) => void;
+    onComplete: (fx: Function, rejectFx?: Function | null) => void;
+    timeout: (ttl: number, expMsg: string | number) => AskObject;
+}
+
 export default askForPromise;
-/**
- * @interface AskObject
- * @description Object with promise and related helper functions
- * @property {Promise} [promise] - Promise object if a single promise is created
- * @property {Array<AskObject>|null} [promises] - Array of promises if multiple promises are created
- * @property {Function} done - Resolve function
- * @property {Function} cancel - Reject function
- * @property {Function} each - Callback function to be called for each list item
- * @property {Function} onComplete - Function to be called after promise is resolved
- * @property {Function} timeout - Function to set timeout on promise
- */
-/**
- * Creates object with promise and related helper functions
- * @function askForPromise
- * @param {Array<any>} [list] - List of items that need to have a corresponding promise.(optional)
- * @returns {AskObject} Object with promise and related helper functions
- */
+
 declare function askForPromise(list?: Array<any>): AskObject;
+
 declare namespace askForPromise {
     /**
-     * @function sequence
-     * @description Executes list of functions that return a promise in sequence.
-     * @param {Array<Function>} list - List of functions that return a promise
-     * @param {...any} args - Arguments to be passed to each function in the list
-     * @returns {AskObject} - Object with promise and related helper functions
+     * Executes list of functions that return a promise in sequence.
+     * @param list - List of functions that return a promise
+     * @param args - Arguments to be passed to each function in the list
+     * @returns Object with promise and related helper functions
      */
     function sequence(list: Array<Function>, ...args: any[]): AskObject;
+
     /**
-     * @function all
-     * @description Executes list of functions that return a promise in parallel.
-     * @param {Array<Function>} list - List of functions that return a promise
-     * @param {...any} args - Arguments to be passed to each function in the list
-     * @returns {AskObject} - Object with promise and related helper functions
+     * Executes list of functions that return a promise in parallel.
+     * @param list - List of functions that return a promise
+     * @param args - Arguments to be passed to each function in the list
+     * @returns Object with promise and related helper functions
      */
     function all(list: Array<Function>, ...args: any[]): AskObject;
 }
-//# sourceMappingURL=askForPromise.d.ts.map
